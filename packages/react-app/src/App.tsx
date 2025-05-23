8acccd12776f9a215f8f8511a0301c3c8186922c
Divyanshu Maithani <div.blackcat@gmail.com>
2025-05-23 18:25:27 +0530

    more local state

--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -11,6 +11,33 @@ function renderCommentsInNode({ comments }) {
   return div;
 }
 
+const Comment = ({ text, author }) => {
+  const [count, setCount] = useState(0);
+  return (
+    <div
+      style={{
+        margin: 4,
+        padding: 4,
+        border: "1px solid hotpink",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          gap: 32,
+        }}
+      >
+        <div style={{ display: "flex", flexDirection: "column", gap: 8 }}>
+          {text} <em>by {author}</em>
+        </div>
+        <button onClick={() => setCount(count + 1)}>{count} ⬆️</button>
+      </div>
+    </div>
+  );
+};
+
 const Comments = ({ comments }) => {
   const [count, setCount] = useState(0);
 
@@ -19,12 +46,11 @@ const Comments = ({ comments }) => {
       Custom Comments UI
       <div>
         {comments.map((comment) => (
-          <div
+          <Comment
             key={comment.id}
-            style={{ margin: 4, border: "1px solid hotpink", padding: 4 }}
-          >
-            {comment.text} - by {comment.author}
-          </div>
+            text={comment.text}
+            author={comment.author}
+          />
         ))}
       </div>
       <button onClick={() => setCount((count) => count + 1)}>
@@ -53,26 +79,11 @@ function App() {
   return (
     <>
       <div ref={sdkContainerRef} />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count in react-app {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
